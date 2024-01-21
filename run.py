0e7cf72a57ec30094b0c8f777b2b7e841d900782
Pedro Sudario <pedrohenriquesudariosilva@gmail.com>
2024-01-21 04:58:01 -0300

    Exception treatment for screen

--- run.py
+++ run.py
@@ -6,7 +6,8 @@ device = device.Device(sys.argv[1])
 try:
     device.connect()
     device.screen.detect_images()
-except:
+except Exception as e:
+    print(e)
     device.disconnect()
 finally:
     device.disconnect()
